bed05a155a77977a55a484188c41e18d4f273575
Julien Olivain <julien.olivain@lsv.ens-cachan.fr>
2012-06-18 00:05:20 +0200

    Wrote a test for chaos_dcsk_mod_cbc.

---
+++ python/qa_chaos.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+#
+# Copyright 2004,2007 Free Software Foundation, Inc.
+#
+# This file is part of GNU Radio
+#
+# GNU Radio is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+#
+# GNU Radio is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GNU Radio; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street,
+# Boston, MA 02110-1301, USA.
+#
+
+from gnuradio import gr, gr_unittest
+#import os, time
+import chaos_swig
+
+class qa_chaos (gr_unittest.TestCase):
+
+    def setUp (self):
+        self.tb = gr.top_block ()
+
+    def tearDown (self):
+        self.tb = None
+
+    def test_001_chaos_dcsk_mod_cbc (self):
+        src_chaos_values = (1+0j, 0+1j, -1+0j, 0-1j)
+        src_data_values = (0, 1, 0, 0, 1, 1)
+        zero = src_chaos_values + tuple(map(lambda v: -v, src_chaos_values))
+        one = 2 * src_chaos_values
+        expected_result = zero + one + zero + zero + one + one
+
+        src_chaos = gr.vector_source_c (src_chaos_values * len(src_data_values))
+        src_data = gr.vector_source_b (src_data_values)
+
+        chaos_mod = chaos_swig.dcsk_mod_cbc(len(src_chaos_values))
+
+        dst = gr.vector_sink_c ()
+
+        self.tb.connect (src_chaos, (chaos_mod,0))
+        self.tb.connect (src_data, (chaos_mod,1))
+        self.tb.connect (chaos_mod, dst)
+
+        self.tb.run ()
+
+        result_data = dst.data ()
+
+        print "expected", expected_result
+        print "result  ", result_data
+
+        self.assertEqual (expected_result, result_data)
+
+
+if __name__ == '__main__':
+    gr_unittest.main ()
